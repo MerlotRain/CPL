be12b4a4d757653953a3562c1c9df19dccaf7d02
qiyuxiang <1198477036@qq.com>
2023-10-11 18:09:21 +0800

    1.add sqlite driver.

---
+++ src/sql/m2_sqldriver.h
@@ -0,0 +1,12 @@
+#ifndef M2_SQLDRIVER_H_
+#define M2_SQLDRIVER_H_
+
+namespace m2 {
+
+class SqlDriver
+{
+};
+
+}// namespace m2
+
+#endif//M2_SQLDRIVER_H_
---
+++ src/sql/sqldrivers/sqlite/m2_sqlitedriver.h
@@ -0,0 +1,14 @@
+#ifndef M2_SQLITEDRIVER_H_
+#define M2_SQLITEDRIVER_H_
+
+#include "../../m2_sqldriver.h"
+
+namespace m2 {
+
+class SqliteDirver : public SqlDriver
+{
+};
+
+}// namespace m2
+
+#endif//M2_SQLITEDRIVER_H_
