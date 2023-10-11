3ae6c6c21faba2558122d6fa10f49c0f9c2e9053
qiyuxiang <1198477036@qq.com>
2023-10-11 17:32:40 +0800

    1.add sql model

--- include/m2/sql/m2_sqldatabase.h
+++ include/m2/sql/m2_sqldatabase.h
@@ -30,283 +30,121 @@
 **
 ****************************************************************************/
 
+#ifndef M2_SQLDATABASE_H_
+#define M2_SQLDATABASE_H_
 
-#pragma once
-
-#include "stringhelp.h"
-#include "variant.h"
-
+#include <m2_string.h>
+#include <m2_variant.h>
 
 namespace m2 {
-namespace Data {
 
+class SqlStatement;
+class SqlDatabasePrivate;
 
-/// @brief 数据库字段类型
-enum SqlFieldType
+enum SqlDataSourceType
 {
-    /// @brief 错误字段类型
-    eErrorType,
-    /// @brief BOOL类型
-    eBoolType,
-    /// @brief 32位有符号整型
-    eInt32Type,
-    /// @brief 32位无符号整型
-    eUInt32Type,
-    /// @brief 64位有符号整型
-    eInt64Type,
-    /// @brief 64位无符号整型
-    eUInt64Type,
-    /// @brief 字符类型
-    eStringType,
-    /// @brief Float类型
-    eFloatType,
-    /// @brief Double类型
-    eDoubleType,
-    /// @brief 日期类型
-    eDateType,
-    /// @brief 二进制类型
-    eBlobType,
-    /// @brief 长文本类型
-    eClobType,
-#ifdef SPATIAL_FEATURE
-    /// @brief 几何字段
-    eGeometryType,
-#endif
+    Oracle,
+    MySql,
+    SQLite,
+    PostgreSQL,
+    ODBC,
+    MSSql,
 };
 
-
-/// @brief 字段结构
-struct M2_API SqlField
+struct SqlConnectionProperty
 {
-    /// @brief 字段名称
-    String Name;
-    /// @brief 字段别名
-    String AliasName;
-    /// @brief 字段类型
-    SqlFieldType Type;
-    /// @brief 字段长度
-    int Length;
-    /// @brief 小数字段精度，整型或字符串字段该值无效
-    int Scale;
-    /// @brief 字段是否允许为空
-    bool IsNullable;
-    /// @brief 字段默认值
-    Variant DefaultValue;
+    String strServer;
+    String strDatabase;
+    String strUser;
+    String strPassword;
+    int nPort;
+    String strOption;
+    int nVersion;
+    SqlDataSourceType dataSourceType;
+};
+typedef void *SqlDatabaseDriver_Handle_t;
 
-    /// @brief 默认构造
+struct SqlField
+{
+    enum Type
+    {
+        eErrorType,
+        eBoolType,
+        eInt32Type,
+        eUInt32Type,
+        eInt64Type,
+        eUInt64Type,
+        eStringType,
+        eFloatType,
+        eDoubleType,
+        eDateType,
+        eBlobType,
+        eClobType,
+    };
+
+    String strName;
+    String strAliasName;
+    Type eType;
+    int nLength;
+    int nScale;
+    bool isNullable;
+    Variant vDefaultValue;
+
     SqlField();
-    /// @brief 拷贝构造函数
-    /// @param rhs
     SqlField(const SqlField &rhs);
+    SqlField(const char *strName, Type eType, const char *strAliasName = 0,
+             const Variant &defaultVal = Variant());
+    SqlField(const char *strName, Type eType, int nPrecision, int nScale = 0,
+             const char *strAliasName = 0, const Variant &defaultVal = Variant());
 
-    /// @brief 从名称和类型构造
-    /// @param strName 字段名称
-    /// @param eType 字段类型
-    /// @param strAliasName 字段别名
-    /// @param defaultVal 字段默认值
-    SqlField(const char *strName, SqlFieldType eType, const char *strAliasName = 0,
-               const Variant &defaultVal = Variant());
-
-    /// @brief 从名称和类型、精度和小数L位构造
-    /// @param strName 字段名称
-    /// @param eType 字段类型
-    /// @param nPrecision 字段精度
-    /// @param nScale 字段小数位长度
-    /// @param strAliasName 字段别名
-    /// @param defaultVal 字段默认值，类型需要与字段类型匹配
-    SqlField(const char *strName, SqlFieldType eType, int nPrecision, int nScale = 0,
-               const char *strAliasName = 0, const Variant &defaultVal = Variant());
-
-    /// @brief 将字段信息序列化为xml字符串
-    /// @return
-    String ToXml() const;
-
-    /// @brief 将字段信息序列化为json字符串
-    /// @return
-    String ToJson() const;
-    /// @brief 相等操作符
+    String toXml() const;
+    String toJson() const;
     bool operator==(const SqlField &field) const;
-    /// @brief 不等操作符
     bool operator!=(const SqlField &field) const;
 };
 
-
-/// @brief 字段集合
-struct M2_API SqlFields
+struct SqlFields
 {
     std::vector<SqlField> Fields;
 
-    /// @brief 寻找字段的索引
-    /// @param strFieldName
-    /// @return
-    int FindField(const char *strFieldName) const;
-
-    /// @brief 将字段信息序列化为xml字符串
-    /// @return
-    String ToXml() const;
-
-    /// @brief 将xml字符串序列化为Fields信息
-    /// @param strInfo
-    bool FromXml(const char *strInfo);
-
-    /// @brief 将字段信息序列化为json字符串
-    /// @return
-    String ToJson() const;
-
-    /// @brief 将json字符串序列化为Fields信息
-    /// @param strInfo
-    bool FromJson(const char *strInfo);
-
-    /// @brief 空字段
-    static const SqlFields &Empty();
+    int findField(const char *strFieldName) const;
+    String toXml() const;
+    bool fromXml(const char *strInfo);
+    String toJson() const;
+    bool fromJson(const char *strInfo);
+    static const SqlFields &empty();
 };
 
-
-class SqlStatement;
-class SqlTransaction;
-
-/// @brief 可直接执行sql语句的普通数据库对象
-/// @details 不使用智能指针封装，对GeoDatabase模块，SqlDatabase指针由内部管理
-/// @details 目前仅对外暴露sqlite数据库的封装
-/// @details 当应用层获取该指针执行sql时，无需析构
-class M2_API SqlDatabase : private NonCopyable
+class SqlDatabase
 {
 public:
-    /// @brief 默认析构
-    virtual ~SqlDatabase() = default;
-    /// @brief 打开数据库
-    /// @return
-    virtual bool Open() = 0;
-    /// @brief 判断数据库是否处于打开的状态
-    /// @return
-    virtual bool IsOpen() const;
-    /// @brief 关闭数据库
-    /// @return
-    virtual bool Close() = 0;
-    /// @brief 开启事务
-    /// @return
-    virtual SqlTransaction *Transaction() = 0;
-    /// @brief 最后一条错误信息
-    /// @return
-    virtual String ErrorMessage();
-    /// @brief 错误代码
-    /// @return
-    virtual long long ErrorCode() = 0;
-    /// @brief 创建Statement对象用于复杂执行
-    /// @return
-    virtual SqlStatement *CreateStatement() = 0;
-    /// @brief 执行sql语句
-    /// @return
-    virtual int Execute(const char *sql) = 0;
+    ~SqlDatabase() = default;
+    bool open(const SqlConnectionProperty &cp);
+    bool isOpen() const;
+    bool close();
+    bool isOpen() const;
+    bool isOpenError() const;
 
-protected:
-    void OpenError(bool b);
-    void LastError(const char *errmsg);
+    SqlConnectionProperty connectionProperty() const;
+    String lastError() const;
+    SqlStatement exec(const char *sql = nullptr);
 
-protected:
-    bool m_isOpen = false;
-    bool m_isOpenError = false;
-    String m_strErrMsg;
+    bool transaction();
+    bool commit();
+    bool rollback();
+
+    static SqlDatabase openDatabase(const SqlConnectionProperty &cp);
+    static SqlDatabase closeDatabase(const SqlConnectionProperty &cp);
+    static void removeDatabase(const SqlConnectionProperty &cp);
+
+    static void registerSqlDrivers(SqlDatabaseDriver_Handle_t t);
+
+    static String createTableSql(const SqlFields &fields, const char *tablename, SqlDataSourceType datasource);
+
+private:
+    SqlDatabasePrivate *d;
 };
 
-
-/// @brief 数据库执行Statement对象
-class M2_API SqlStatement : private NonCopyable
-{
-protected:
-    SqlDatabase *m_pDB;
-    String m_strSQL;
-
-public:
-    /// @brief 缺省构造函数
-    SqlStatement();
-    /// @brief 重载构造函数，创建数据库指针
-    SqlStatement(SqlDatabase *db, const char *strSQL);
-    virtual ~SqlStatement();
-    virtual bool Prepare(SqlDatabase *db, const char *sql) = 0;
-    /// @brief 将本地字符串转化为数据库字符串
-    virtual String SQLString();
-    /// @brief 返回-1
-    virtual long long RecordCount();
-    /// @brief 读取32位，nCol从0开始，下同
-    virtual int IntValue(int nCol);
-    /// @brief 读取64位
-    virtual long long Int64Value(int nCol);
-    /// @brief 读取字符串
-    virtual String StringValue(int nCol);
-    /// @brief 读取日期
-    virtual DateTime DataTimeValue(int nCol);
-    /// @brief 读取无符号字符常量
-    virtual const char *StringValuePtr(int nCol);
-    /// @brief 读取double类型数据
-    virtual double DoubleValue(int nCol);
-    /// @brief 判断列是否是空值
-    virtual bool IsNullColunm(int n);
-
-    /// @brief 绑定无符号字符, nIndex从0开始
-    virtual int Bind(int nIndex, const unsigned char *pBlob, int nLen);
-    /// @brief 绑定32位整数
-    virtual int Bind(int nIndex, int nValue);
-    /// @brief 绑定64位浮点数
-    virtual int Bind(int nIndex, double nValue);
-    /// @brief 绑定64位无符号整数
-    virtual int Bind(int nIndex, unsigned long long nValue);
-    /// @brief 绑定空值
-    virtual int Bind(int nIndex);
-    /// @brief 绑定字符常量
-    virtual int Bind(int nIndex, const char *pStr);
-    /// @brief 绑定64位整数或者日期
-    virtual int Bind(int nIndex, long long nValue, bool bIsDateTime = false);
-
-public:
-    /// @brief 执行读取，返回1个结果
-    virtual int ExecuteQuery();
-    /// @brief 执行读取，返回多个结果
-    virtual int Execute();
-    /// @brief 返回下1条数据
-    virtual bool MoveNext() = 0;
-    /// @brief 读取无符号字符数据
-    virtual const unsigned char *BlobValue(int nCol) = 0;
-    /// @brief 返回当前列的值
-    virtual int ValueLength(int nCol) = 0;
-    /// @brief 绑定索引,nIndex 从0开始
-    virtual int BindValue(int nIndex, const unsigned char *pBlob, int nLen, SqlFieldType eType) = 0;
-    /// @brief 释放内存
-    virtual int Reset() = 0;
-    /// @brief 获取查询结果的列数
-    virtual int ColumnCount() = 0;
-    /// @brief 获取列的类型
-    virtual SqlFieldType ColunmType(int n) = 0;
-    /// @brief 获取列的名称
-    virtual String ColunmName(int n) = 0;
-};
-
-
-/// @brief 数据库事务对象
-class M2_API SqlTransaction : public RefObject
-{
-public:
-    /// @brief 默认析构
-    virtual ~SqlTransaction() {}
-    /// @brief 是否处于事务中
-    /// @return 返回是否处于事务中
-    virtual bool IsTransaction() = 0;
-    /// @brief 启动事务
-    /// @return 返回是否成功启动
-    virtual bool StartTransaction() = 0;
-    /// @brief 提交事务
-    /// @return 返回是否提交成功
-    virtual bool CommitTransaction() = 0;
-    /// @brief 取消事务
-    /// @return 返回是否取消成功
-    virtual bool RollbackTransaction() = 0;
-
-protected:
-    /// @brief 默认构造
-    SqlTransaction() {}
-};
-M2_SMARTER_PTR(SqlTransaction)
-
-
-}// namespace Data
 }// namespace m2
+
+#endif//M2_SQLDATABASE_H_
--- include/m2/sql/m2_sqlstatement.h
+++ include/m2/sql/m2_sqlstatement.h
@@ -29,3 +29,63 @@
 ** $M2_END_LICENSE$
 **
 ****************************************************************************/
+
+#ifndef M2_SQLSTATEMENT_H_
+#define M2_SQLSTATEMENT_H_
+
+#include <m2_sqldatabase.h>
+
+namespace m2 {
+
+class ByteBuffer;
+class SqlStatementPrivate;
+class SqlStatement
+{
+protected:
+    SqlDatabase *m_pDB;
+    String m_strSQL;
+
+public:
+    SqlStatement();
+    SqlStatement(SqlDatabase *db, const char *strSQL);
+    virtual ~SqlStatement();
+    virtual bool prepare(SqlDatabase *db, const char *sql) = 0;
+    String sqlString();
+    long long recordCount();
+    Variant value(int n) const;
+    Variant value(const String &name) const;
+
+    bool isNullColunm(int n);
+
+    int bindValue(int nIndex, const unsigned char *pBlob, int nLen);
+    int bindValue(int nIndex, int nValue);
+    int bindValue(int nIndex, double nValue);
+    int bindValue(int nIndex, unsigned long long nValue);
+    int bindValue(int nIndex);
+    int bindValue(int nIndex, const char *pStr);
+    int bindValue(int nIndex, long long nValue, bool bIsDateTime = false);
+
+    int bindValue(const String &placeholder, const unsigned char *pBlob, int nLen);
+    int bindValue(const String &placeholder, int nValue);
+    int bindValue(const String &placeholder, double nValue);
+    int bindValue(const String &placeholder, unsigned long long nValue);
+    int bindValue(const String &placeholder);
+    int bindValue(const String &placeholder, const char *pStr);
+    int bindValue(const String &placeholder, long long nValue, bool bIsDateTime = false);
+
+public:
+    bool exec();
+    bool next();
+    bool previous();
+    bool first();
+    bool last();
+    int reset();
+
+    virtual int columnCount() = 0;
+    virtual SqlField::Type colunmType(int n) = 0;
+    virtual String colunmName(int n) = 0;
+};
+
+}// namespace m2
+
+#endif//M2_SQLSTATEMENT_H_
